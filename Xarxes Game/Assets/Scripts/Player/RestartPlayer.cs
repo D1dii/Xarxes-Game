0ca2768da891df22c1bb974a4e9c3d9dfb230585
D1dii <didacgp123@gmail.com>
2025-11-04 15:59:28 +0100

    Death Zone

---
+++ Xarxes Game/Assets/Scripts/Player/RestartPlayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RestartPlayer : MonoBehaviour
+{
+
+    public Vector3 startPos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("DeathZone"))
+        {
+            RestartPosition();
+        }
+    }
+
+    public void RestartPosition()
+    {
+        transform.position = startPos;
+    }
+}
