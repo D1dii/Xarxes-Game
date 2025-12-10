b3e715da7a6c6852b733dba988c445800572a95d
Biel <145224865+Drauguer@users.noreply.github.com>
2025-12-10 11:58:49 +0100

    level 1 fix

--- Xarxes Game/Assets/Scripts/Level Objects/MovingWall.cs
+++ Xarxes Game/Assets/Scripts/Level Objects/MovingWall.cs
@@ -8,17 +8,15 @@ public class MovingWall : MonoBehaviour
 
     public float speed = 1f;
 
-    private NetworkObject netObj;
 
     private void Awake()
     {
-        netObj = GetComponent<NetworkObject>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!netObj.isLocalPlayer) return;
         float t = Mathf.PingPong(Time.time * speed, 1f);
         transform.localPosition = Vector3.Lerp(startPos, endPos, t);
     }
